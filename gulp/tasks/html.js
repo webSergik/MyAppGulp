3031d1724192db96a8ae5217a293d30840e7c18e
webSergik <serg47181@gmail.com>
2015-06-21 16:11:47 +0500

    myApp version 0.2

---
+++ gulp/tasks/html.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+
+gulp.task('html', function() {
+  gulp.src('./app/*.html')
+    .pipe(gulp.dest('./public/'))
+    .pipe(browserSync.reload({stream: true}));
+});
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,4 +3,5 @@ var gulp = require('gulp');
 gulp.task('watch', function () {
 	gulp.watch("./app/sass/**/*.scss", ['sass']);
 	gulp.watch("./app/jade/**/*.jade", ['jade']);
+	gulp.watch("./app/*.html", ['html']);
 });
